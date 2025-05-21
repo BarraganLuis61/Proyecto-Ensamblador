9828b3029c7e0cbd26f803f1e2b5c0bc4c6718ff
LuisAngelBarraganGonzalez <al541664@edu.uaa.mx>
2025-05-21 14:07:07 -0600

    Create proyecto.asm
    Logica del laberinto en lenguaje ensamblador

---
+++ proyecto.asm
@@ -0,0 +1,9 @@
+section .text
+  global mover_jugador
+
+;Constantes
+%define FILAS 10
+%define COLUMNAS 12
+
+;int mover_jugador(char laberinto[FILAS][COLUMNAS],int x, int y, char letra)
+;Windows x64 calling convention:
